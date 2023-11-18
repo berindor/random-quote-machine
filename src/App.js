d91bfc6be61811fd43e0bd4fff87bf6f62d76aba
Beringer Dorottya <beringerdorottya@gmail.com>
2023-11-18 18:17:57 +0100

    add twitter icon + some refactoring

--- src/App.js
+++ src/App.js
@@ -1,51 +1,57 @@
 
 import './App.css';
 import React from 'react';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
-
+function getRandomColor() {
+  const hue = Math.floor(Math.random() * 360);
+  const saturation = Math.floor(25 + Math.random() * 50);
+  const quoteBoxColor = `hsl(${hue},${saturation}%,70%)`;
+  const backgroundColor = `hsl(${hue},${saturation}%,90%)`;
+  return {
+    quoteBox: quoteBoxColor,
+    background: backgroundColor
+  }
+};
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       randomNumber: 0,
-      quote: 'Default quote',
-      author: 'Default Author',
-      color: 'rgb(200, 50, 100)',
-      quoteIsRandom: false
+      quote: '',
+      author: '',
+      color: getRandomColor()
     }
     this.loadNewQuote = this.loadNewQuote.bind(this);
-    }
+  }
+
+  componentDidMount() {
+    this.loadNewQuote();
+  };
 
   async loadNewQuote() {
-    const randomColor = 'hsl(' + Math.floor(Math.random() * 360) + ',' + Math.floor(25 + Math.random() * 50) + '%,70%)';
+    const randomColor = getRandomColor();
     const response = await fetch("https://api.quotable.io/quotes/random");
     const [quote] = await response.json();
     return this.setState( {
       quote: quote.content,
       author: quote.author,
-      color: randomColor,
-      quoteIsRandom: true
+      color: randomColor
     } );
   }
   
   render() {
-    if (this.state.quoteIsRandom === false) {
-      this.loadNewQuote();
-    } // something is wrong, loads twice. What is the right way to load a random quote first? componentDidMount didn't work (the way I tried)
-    const quoteText = this.state.quote;
-    const quoteAuthor = this.state.author;
-    const randomBackgroundArr = this.state.color.split(',').slice(0,-1);
-    randomBackgroundArr.push('90%)');
-    const randomBackground = randomBackgroundArr.join();
     return (
-      <div id="body-div" style={{backgroundColor: randomBackground }} >
-        <div id="quote-box" style={{backgroundColor: this.state.color }}>
-          <div id="text"><span className="quote-mark">“</span>{quoteText}<span className="quote-mark">”</span></div>
-          <div id="author">- {quoteAuthor}</div>
+      <div id="body-div" style={{backgroundColor: this.state.color.background }} >
+        <div id="quote-box" style={{backgroundColor: this.state.color.quoteBox }}>
+          <div id="text"><span className="quote-mark">“</span>{this.state.quote}<span className="quote-mark">”</span></div>
+          <div id="author">- {this.state.author}</div>
           <div id="bottom-div">
-            <button id="new-quote" style={{color: this.state.color }} onClick={this.loadNewQuote} >New quote</button>
-            <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank" >twitter icon</a>
+            <button id="new-quote" style={{color: this.state.color.quoteBox }} onClick={this.loadNewQuote} >New quote</button>
+            <a id="tweet-quote" href="https://twitter.com/intent/tweet" target="_blank" >
+              <TwitterIcon id="twitterIcon" style={{color: this.state.color.quoteBox }}/>
+            </a>
           </div>
         </div>
       </div>
